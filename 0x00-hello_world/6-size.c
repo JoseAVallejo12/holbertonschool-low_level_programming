416a7161cffec587c53e97c708d61ebc08df1013
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-02-05 22:05:40 +0000

    edit task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,10 @@
-#include <stdio.h>
+#include<stdio.h>
 
 /**
  * main - Entry point
- * task 6..
+ * 
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 printf("Size of a char: %ld byte(s)\n", sizeof(char));
