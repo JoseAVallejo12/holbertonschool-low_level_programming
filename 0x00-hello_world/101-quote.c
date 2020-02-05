6dc45e7a6393d5203075f50e3e70da80d15d0695
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-02-05 23:16:09 +0000

    create task 101

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,9 +8,9 @@
  */
 int main(void)
 {
-int l;
-l = sizeof("and that piece of art is useful - Dora Korpar, 2015-10-19");
-write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", l);
+int size;
+size = sizeof("and that piece of art is useful - Dora Korpar, 2015-10-19");
+write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19", size);
 
 return (1);
 }
