778de79e92d117e455d27d4a22f3ab724b930508
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-02-05 23:10:37 +0000

    create task 101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int l;
+l = sizeof("and that piece of art is useful - Dora Korpar, 2015-10-19");
+write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", l);
+
+return (1);
+}
