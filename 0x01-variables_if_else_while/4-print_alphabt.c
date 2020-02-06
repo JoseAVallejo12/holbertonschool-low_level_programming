433aa3a2d9199e5756db743b73c3bece548231c4
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-02-06 20:38:58 +0000

    edit task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+for (i = 97; i <= 122; i++)
+{
+if ((i != 101) && (i != 113))
+{
+putchar(i);
+}
+}
+
+putchar(10);
+
+return (0);
+}
