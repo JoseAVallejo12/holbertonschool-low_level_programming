4a81ad24348e2f144c1a371adb3eee8756be967d
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-02-06 21:58:37 +0000

    edit task 10

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i,j;
+for (i = 48; i <= 57; i++)
+{
+for (j = 48; j <= 57; j++)
+{
+putchar(i);
+putchar(j);
+if ((i == 57)&&(j == 57))
+{
+break;
+}
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+
+return (0);
+}
