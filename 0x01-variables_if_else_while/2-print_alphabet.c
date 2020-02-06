d6c58b9e4edd79e4602550caffe87e887ad64a5c
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-02-06 19:58:29 +0000

    edit task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+
+for (i = 'a'; i <= 'z'; i++)
+{
+putchar(i);                                                                     }
+
+putchar('\n');
+
+return (0);
+}
