3fd1670761c0fb7a1024ae6cfa151a6a045166e3
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-02-07 03:46:51 +0000

    edit task 101

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,38 +7,37 @@
  */
 int main(void)
 {
-int x, a;
-int y, b;
-int z, c;
+int x;
+int y;
+int b;
+int z;
+int c;
 
-a = 48;
 b = 49;
 c = 50;
-for (x = a; x < 56; x++)
-{
-for (y = b; y < 57; y++)
-{
-for (z = c; z < 58; z++)
-{
-if (x != y && x != z && y != z)
-{
-putchar(x);
-putchar(y);
-putchar(z);
-if (x == 55 && y == 56 && z == 57)
-{
-continue;
-}
-putchar(44);
-putchar(' ');
-}
-c = c + 1;
-}
-c = 51;
-b = b + 1;
-}
-b = 50;
-}
-putchar(10);
-return (0);
-}
+for (x = 48; x < 56; x++)
+	{
+	b = 1 + x;
+	for (y = b; y < 57; y++)
+	{
+		c = 1 + y;
+		for (z = c; z < 58; z++)
+		{
+			if (x != y && x != z && y != z)
+			{
+				putchar(x);
+				putchar(y);
+				putchar(z);
+				if (x == 55 && y == 56 && z == 57)
+				{
+					continue;
+				}
+				putchar(44);
+				putchar(' ');
+			}
+		}
+	}
+	}
+	putchar(10);
+	return (0);
+	}
