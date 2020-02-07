ecf40655c985a2ca64cc204e4d5d1d0ac6c25768
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-02-07 04:35:55 +0000

    edit task 102

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,27 +7,27 @@
  */
 int main(void)
 {
-    int x, y;
-    for (x = 0; x <= 99; x++)
-    {
-        for(y = 0; y <= 99; y++)
-        {
-            if (x < y)
-            {
-                putchar(x / 10 + '0');
-                putchar(x % 10 + '0');
-                putchar(32);
-                putchar(y / 10 + '0');
-                putchar(y % 10 + '0');
+	int x, y;
 
-                if (x != 98)
-                {
-                    putchar(',');
-                    putchar(' ');
-                }
-            }
-        }
-    }
-putchar(10);
-return (0);
+	for (x = 0; x <= 99; x++)
+	{
+		for (y = 0; y <= 99; y++)
+		{
+			if (x < y)
+			{
+				putchar(x / 10 + '0');
+				putchar(x % 10 + '0');
+				putchar(32);
+				putchar(y / 10 + '0');
+				putchar(y % 10 + '0');
+				if (x != 98)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar(10);
+	return (0);
 }
