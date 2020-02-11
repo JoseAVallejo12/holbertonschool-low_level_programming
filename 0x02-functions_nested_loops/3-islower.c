8649b493b4fca4f2166dfd8973b89580396e3603
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-02-10 23:35:42 -0500

    create prototype task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include<ctype.h>
+#include"holberton.h"
+
+/**
+ *print_alphabet_x10 - 2-alphabet
+ *Return: void
+ */
+
+int _islower(int c)
+{
+	int n;
+	n = islower(c);
+	printf("valor de n %d,n);
+	return (n);
+}
+
