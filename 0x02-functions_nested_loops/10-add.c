1ca082fe4515a2f8fa83a7aee77231b4f1f399d4
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-02-11 22:25:47 -0500

    cerate

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * add - check the code for Holberton School students.
+ *@x: is the firts parameter.
+ *@y: is the second parameter.
+ * Return: Always 0.
+ */
+int add(int x, int y)
+{
+	int c;
+
+	c = x + y;
+
+	return (c);
+
+}
