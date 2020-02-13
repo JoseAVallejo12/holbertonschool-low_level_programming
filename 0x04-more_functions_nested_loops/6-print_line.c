03754d9fbcb52de65b7e46bccc48227ea8a9c88c
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-02-13 11:53:11 -0500

    create, edit and cheker task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ *print_line - Print straigth line n time
+ *@n: Parameter
+ *Return: always return zero
+ */
+
+void print_line(int n)
+{
+
+	if (n > 0)
+	{
+		while (--n)
+			_putchar(95);
+	}
+	_putchar('\n');
+}
