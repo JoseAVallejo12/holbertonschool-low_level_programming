0d78d78b3d5f6d2b53735ea6c0021680886ed705
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-02-13 14:24:53 -0500

    delete funtion myprint

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,35 +1,29 @@
 #include "holberton.h"
-/**
- *myprint - ptint n times
- *@n: Parameter
- *Return: always void
- */
-void myprint(int n)
-{
-	int i;
-
-	for (i = 0; i <= n; i++)
-		_putchar(' ');
-}
 
 /**
  *print_diagonal - Print straigth line n time
  *@n: Parameter
  *Return: always return zero
  */
+
 void print_diagonal(int n)
 {
-	int i;
+	int i, e;
 
 	if (n <= 0)
+	{
 		_putchar('\n');
+	}
 
 	i = 0;
 	if (n > 0)
 	{
 		while (i != n)
 		{
-			myprint(i);
+			for (e = 0; e <= i; e++)
+			{
+				_putchar(' ');
+			}
 			_putchar(92);
 			_putchar('\n');
 			i++;
