fad01ead94bb79c0d3c02e0238d7a2e08f74cc95
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-02-13 11:16:42 -0500

    create, edit and checker task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+
+/**
+ *more_numbers - Print number of 0 to 9 and exclude 2, and 4
+ *Return: always void
+ */
+
+void more_numbers(void)
+{
+	int a, b, c, d;
+
+	a = 0;
+	b = 0;
+	while (a < 11)
+	{
+		c = 0;
+		d = 0;
+		while (b <= 14)
+		{
+			c = b / 10;
+			d = b % 10;
+			if (b <= 9)
+				_putchar(b + 48);
+			if (b > 9)
+			{
+				_putchar(c + 48);
+				_putchar(d + 48);
+			}
+			b++;
+		}
+		_putchar('\n');
+		b = 0;
+		a++;
+	}
+}
+
