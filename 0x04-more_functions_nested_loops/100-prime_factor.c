82eb350280733083d6d79ac7718a7660be439f0b
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-02-13 19:08:17 -0500

    create task 100

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ *main - print whint printf funtion
+ *
+ *Return: Always
+ */
+
+int main(void)
+{
+	long int n;
+
+	n = 612852475143;
+	long int div = 2, ans = 0, maxFact;
+
+	while (n != 0)
+	{
+		if (n % div != 0)
+			div = div + 1;
+		else
+		{
+			maxFact = n;
+			n = n / div;
+			if (n == 1)
+			{
+				printf("%ld is the largest prime factor !", maxFact);
+				ans = 1;
+				break;
+			}
+		}
+	}
+	return (0);
+}
