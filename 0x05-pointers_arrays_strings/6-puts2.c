582910f23316e0ec93a7bfe1753da9788a596c0f
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-02-18 14:40:24 -0500

    create task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ *puts2 - check the code for Holberton School students.
+ *@str: Parameter
+ *
+ * Return: Always 0.
+ */
+
+void puts2(char *str)
+{
+	int n;
+
+	n = 0;
+	while (str[n] != '\0')
+	{
+		_putchar(str[n]);
+		n += 2;
+	}
+	_putchar('\n');
+
+}
+
