c6f7d4d7c4db3bfb5e8ae12edf68d8c1b5ad8318
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-02-18 15:56:24 -0500

    add {} to funtion if

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -15,10 +15,14 @@ void puts_half(char *str)
 	while (str[n] != '\0')
 		n++;
 
-	if (n % 2 != 0)
-		n = (n - 1) / 2;
-	else
+	if (n % 2 == 0)
+	{
 		n = n / 2;
+	}
+	else
+	{
+		n = (n - 1) / 2;
+	}
 
 	while (str[n] != '\0')
 	{
