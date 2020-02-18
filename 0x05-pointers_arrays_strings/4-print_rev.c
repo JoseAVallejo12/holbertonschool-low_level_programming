58c8b5440eddd8a5f6b1b9e39e41ee265c494d61
JVC_XioamiCellphone <josealfredovallejo25@gmail.com>
2020-02-18 05:41:00 -0500

    create task 5

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ *rev_string - check the code for Holberton School students.
+ *@s: Parameters*
+ * Return: Always void.
+ */
+
+void rev_string(char *s)
+{
+	int n, i;
+
+	n = 0;
+	while (s[n] != '\0')
+	}
+		n++;
+	
+	}
+	for (i = n; i <= 0; i--)
+	 	_putchar('\n');
+
+
+
+}
+
