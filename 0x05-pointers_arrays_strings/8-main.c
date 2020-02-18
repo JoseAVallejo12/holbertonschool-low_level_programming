01d6c2c048608d13c4e93b8a819330139120ba82
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-02-18 17:47:39 -0500

    create task 8

--- 0x05-pointers_arrays_strings/8-main.c
+++ 0x05-pointers_arrays_strings/8-main.c
@@ -17,3 +17,4 @@ int main(void)
 	print_array(array, 5);
 	return (0);
 }
+
---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ *print_array - Prints half of a string
+ *@a: Punter
+ *@n: integer
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	i = 0;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if(i != 4)
+		{
+			printf(",");
+			printf(" ");
+		}
+
+	}
+	putchar('\n');
+}
+
