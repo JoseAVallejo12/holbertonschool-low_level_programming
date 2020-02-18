73380c39ceea327bc499fb2721b451ef8ebfd58c
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-02-18 15:05:47 -0500

    create task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ *puts_half - check the code for Holberton School students.
+ *@str: Parameter
+ *
+ * Return: Always 0.
+ */
+
+void puts_half(char *str)
+{
+	int n, m;
+
+	n = 0;
+	m = 0;
+	while (str[n] != '\0')
+		n++;
+	m = n / 2;
+	while (str[m] != '\0')
+	{
+		_putchar(str[m]);
+		m++;
+
+	}
+	_putchar('\n');
+
+}
+
