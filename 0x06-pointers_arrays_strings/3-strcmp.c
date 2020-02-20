fb1a98eeb38ef04dca3d21dd2656decce62a5675
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-02-20 17:10:53 -0500

    create file task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ *_strcmp - funtion compare two string
+ *@s1: Punter one
+ *@s2: Punter two
+ *Return: integer
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	char c1, c2;
+
+	do
+	{
+		c1 =  *s1++;
+		c2 =  *s2++;
+		if (c1 == '\0')
+			return c1 - c2;
+	}
+	while (c1 == c2);
+	return c1 - c2;
+}
+
--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -1,3 +1,5 @@
 char *_strcat(char *, char *);
 char *_strncat(char *, char *, int);
 char *_strncpy(char *, char *, int);
+int _strcmp(char *, char *);
+
