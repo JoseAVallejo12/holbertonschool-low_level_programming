0e1b863af0e90530ccef2d037311330bb8e66b06
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-02-20 17:15:25 -0500

    checker with betty all done

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,14 +11,13 @@ int _strcmp(char *s1, char *s2)
 {
 	char c1, c2;
 
-	do
-	{
+	do {
 		c1 =  *s1++;
 		c2 =  *s2++;
 		if (c1 == '\0')
-			return c1 - c2;
-	}
-	while (c1 == c2);
-	return c1 - c2;
+			return (c1 - c2);
+
+	} while (c1 == c2);
+	return (c1 - c2);
 }
 
