b4acc42286026bbd287f57bf645458bd3b3b4715
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-02-24 15:35:24 -0500

    create task 5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ *_strstr - find
+ *@haystack: initial segment
+ *@needle: prefix
+ *Return: Always char
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j;
+
+	for (i = 0; needle[i]; i++)
+	{
+		for (j = 0; haystack[j]; j++)
+		{
+			if (needle[i] == haystack[j])
+				return (haystack + j);
+		}
+	}
+
+	return (0);
+}
+
