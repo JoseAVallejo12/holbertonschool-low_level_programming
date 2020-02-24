67d92052cf58517d1734613d669ffa197019c9e8
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-02-24 15:41:53 -0500

    edit task 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -11,9 +11,9 @@ char *_strstr(char *haystack, char *needle)
 {
 	int i, j;
 
-	for (i = 0; needle[i]; i++)
+	for (i = 0; needle[i] != '\0'; i++)
 	{
-		for (j = 0; haystack[j]; j++)
+		for (j = 0; haystack[j] != '\0'; j++)
 		{
 			if (needle[i] == haystack[j])
 				return (haystack + j);
