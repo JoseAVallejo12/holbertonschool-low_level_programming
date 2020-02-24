e5a8fc7cccb6be11314f31e83dc9c7a6196049fe
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-02-24 10:56:41 -0500

    create and checker task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ *_memset - copy n positions of b in *s
+ *@s: pointer dest
+ *@b: array source
+ *@n: parameters
+ *Return: Always string
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+	char *dest, src;
+
+	dest = s;
+	src = b;
+
+	for (i = 0; i < n; i++)
+		*(dest + i) = src;
+
+	return (dest);
+}
