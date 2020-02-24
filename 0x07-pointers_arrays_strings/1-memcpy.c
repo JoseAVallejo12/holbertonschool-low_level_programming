114390d7c1a1ecf389915412a8b187fd243b512c
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-02-24 11:05:00 -0500

    create and checker tasks 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,20 +1,23 @@
 #include "holberton.h"
 
 /**
- *_memset - copy n positions of b in *s
- *@s: pointer dest
- *@b: array source
+ *_memcpy - copy n positions of b in *s
+ *@dest: pointer dest
+ *@src: array source
+ *@n: Parameter unsigned
  *Return: Always string
  */
 
-char *_memset(char *s, char b, unsigned int n)
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
-	char *dest, *src;
+	char *d, *s;
 
-	dest = s;
-	src = b
+	d = dest;
+	s = src;
 
-		for (i = 0; i < n; i++)
-			*(d + i) = *(s +i);
+	for (i = 0; i < n; i++)
+		*(d + i) = *(s + i);
+
+	return (d);
 }
--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -1,1 +1,3 @@
-char *_memset(char *s, char b, unsigned int n);
+char *_memset(char *, char, unsigned int);
+char *_memcpy(char *, char *, unsigned int);
+
