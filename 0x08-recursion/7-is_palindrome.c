4a20e656dc84dc0744c77d39b726f09ae9b34b7c
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-02-27 18:06:24 -0500

    create task 7

--- 0x08-recursion/7-is_palindrome.c
+++ 0x08-recursion/7-is_palindrome.c
@@ -1,20 +1,52 @@
 #include "holberton.h"
 
+/**
+ *len - givin legenth of the string
+ *@s: string
+ *Return: Always integer
+ */
+
 int len(char *s)
 {
-	int i = 0;
-	while(*(s++))
-		i++
+	if ((*s) == '\0')
+		return (0);
+	return (1 + len(s + 1));
 }
+
 /**
- *is_palindorme - check palindrome world
+ *palindrome - checker if an string is palindrome
+ *@s: string
+ *@size: legenth of the string
+ *Return: Always an integer
+ */
+
+int palindrome(char *s, int size)
+{
+	if (size <= 0)
+		return (1);
+
+	else if (*s == *(s + size))
+		return (palindrome(s + 1, size - 2));
+
+	return (0);
+}
+
+/**
+ *is_palindrome - check palindrome world
  *@s: Array in
- *REturn: Always integer
+ *Return: Always integer
  *Descripcion: If s is palindrome return 1 else 0
  */
 
 int is_palindrome(char *s)
 {
-	int count;
-	char *start, *end;
-	if (count
+	int size;
+
+	if (*(s) == '\0')
+		return (0);
+
+	size = len(s);
+
+	return (palindrome(s, size - 1));
+
+}
