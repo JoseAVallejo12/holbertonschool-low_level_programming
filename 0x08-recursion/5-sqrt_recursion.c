7c1164f0d08cefb579fed77d78dcbe2329441879
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-02-26 19:34:14 -0500

    edit task 5

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+/**
+ *root - find raiz of number n
+ *@n: number
+ *@a: interacion
+ *Return: Always integer
+ */
+
+int root(int a, int n)
+{
+	if (a * a  == n)
+		return (a);
+	else if (a * a > n)
+		return (-1);
+	return (root(a + 1, n));
+}
+
+/**
+ *_sqrt_recursion - square natural of a number
+ *@n: base number
+ *Return: Always integer
+ */
+
+int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (0);
+
+	return (root(1, n));
+
+}
+
--- 0x08-recursion/holberton.h
+++ 0x08-recursion/holberton.h
@@ -5,4 +5,5 @@ void _print_rev_recursion(char *);
 int _strlen_recursion(char *s);
 int factorial(int);
 int _pow_recursion(int, int);
+int _sqrt_recursion(int n);
 
