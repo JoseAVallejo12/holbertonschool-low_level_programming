ccfedcaa50158ebab6ea8e13f8340b5cd95d5651
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-02-29 08:26:40 -0500

    create and checker task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,53 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ *_atoi - back int of the string
+ *@s: Punter char
+ *Return: integer number
+ *Descripcion:
+ *Write a function that convert a string to an integer.
+ */
+
+int _atoi(char *s)
+{
+	unsigned int n, num;
+	int sig;
+
+	n = 0;
+	num = 0;
+	sig = 1;
+
+	while (s[n] != '\0')
+	{
+		if (s[n] == '-')
+			sig *= -1;
+
+		if (s[n] >= '0' && s[n] <= '9')
+		{
+			num = num * 10 + (s[n] - '0');
+			if (s[n + 1] == ' ')
+				break;
+		}
+		n++;
+	}
+	return (num * sig);
+}
+
+/**
+ *main - funtion
+ *@argc: legenth of argv
+ *@argv: number of argummet
+ *Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc <= 1)
+		printf("Error\n");
+	else
+		printf("%d\n", _atoi(argv[1]) * _atoi(argv[2]));
+
+	return (0);
+}
+
