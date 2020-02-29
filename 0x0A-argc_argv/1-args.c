97721a10089f408fade16f5a481fa432007f95b8
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-02-28 19:12:47 -0500

    create and checker done

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ *main - funtion
+ *@argc: legenth of argv
+ *@argv: number of argummet
+ *Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+
+	(void) argv;
+
+	_putchar((argc - 1) + '0');
+	_putchar('\n');
+
+	return (0);
+}
