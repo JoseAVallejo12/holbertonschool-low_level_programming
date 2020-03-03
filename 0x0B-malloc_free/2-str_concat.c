c59a58b4f108ab08de6ac57de9541df91ae0db0b
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-03-03 16:03:51 -0500

    edit

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -30,7 +30,7 @@ char *str_concat(char *s1, char *s2)
 	if ( s2 == NULL)
 		s2 = " ";
 
-	size = _strlen(s1) * _strlen(s2) + 1;
+	size = _strlen(s1) + _strlen(s2) + 1;
 
 	arr = (char *)malloc(size * sizeof(char));
 
