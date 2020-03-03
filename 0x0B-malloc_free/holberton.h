4991e8b18bc3624385e3123efcbb6c0edce4ed7b
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-03-03 11:56:57 -0500

    add librey

--- 0x0B-malloc_free/holberton.h
+++ 0x0B-malloc_free/holberton.h
@@ -1,4 +1,8 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
+#include <stdlib.h>
+#include <stdio.h>
 char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
 #endif /* HOLBERTON */
+
