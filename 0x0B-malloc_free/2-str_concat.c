6ff2be1e3ee95de3c6dcdf5448cb5eb831030040
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-03-03 16:13:38 -0500

    new revision

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -26,9 +26,9 @@ char *str_concat(char *s1, char *s2)
 	unsigned int i, j, size;
 
 	if (s1 == NULL)
-		s1 = " ";
+		s1 = "";
 	if (s2 == NULL)
-		s2 = " ";
+		s2 = "";
 
 	size = _strlen(s1) + _strlen(s2) + 1;
 
