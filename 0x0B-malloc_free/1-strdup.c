47dd217b686321765a44a35b9ed3e07cff35d7e9
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-03-03 14:35:58 -0500

    edit and checker done

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,43 @@
+#include "holberton.h"
+
+/**
+ *_strlen - giving to array
+ *@s: pointer to array
+ *Return: Always an int
+ */
+
+int _strlen(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen(s + 1));
+}
+
+/**
+ *strdup - back a pointer to array
+ *@str: pointer to array
+ *Return: Always an array dinamic
+ */
+
+char *_strdup(char *str)
+{
+	char *arr;
+	unsigned int i, size;
+
+	if (str == 0)
+		return (NULL);
+
+	size = _strlen(str) + 1;
+
+	arr = (char *)malloc(size * sizeof(char));
+
+	if (str == 0)
+		return (NULL);
+
+	for (i = 0; *(str + i) != '\0'; i++)
+		*(arr + i) = *(str + i);
+
+	*(arr + i) = 0;
+
+	return (arr);
+}
