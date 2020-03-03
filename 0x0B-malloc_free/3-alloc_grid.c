03c6eda8b1298fefbf33b5b05fd85f57905292a7
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-03-03 17:59:53 -0500

    edit

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -15,18 +15,26 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 
 	arr2d = (int **)malloc(height * sizeof(int *));
+	if (arr2d == NULL)
+	{
+		free(arr2d);
+		return (0);
+	}
 
 	for (i = 0; i < height; i++)
 	{
 		arr2d[i] = malloc(width * sizeof(int *));
+		if(arr2d[i] == NULL)
+		{
+			free(arr2d);
+			return (0);
+		}
 	}
+
 	for (i = 0; i < height; i++)
 	{
 		for (j = 0; j < width; j++)
-		{
 			arr2d[i][j] = 0; /* Each element set to 0 */
-		}
-
 	}
 
 	return (arr2d);
