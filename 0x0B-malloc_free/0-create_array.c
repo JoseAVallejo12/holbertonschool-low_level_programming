c7cedb7ae40c16ae38ec657d2025ab40f01f2c13
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-03-03 12:49:51 -0500

    validate null condicion

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -13,9 +13,13 @@ char *create_array(unsigned int size, char c)
 	unsigned int i;
 
 	if (size == 0)
-		return (0);
+		return (NULL);
 
 	A = (char *)malloc(size * sizeof(c));
+
+	if (A == 0)
+		return (NULL);
+
 	for (i = 0; i < size; i++)
 		*(A + i) = c;
 
