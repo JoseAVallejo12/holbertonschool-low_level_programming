12f700db067e7b0a07598c0d21042eb1e92f35d8
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-03-03 18:43:03 -0500

    edit task

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -9,7 +9,10 @@
 
 int **alloc_grid(int width, int height)
 {
-	int **arr, i, j;
+	int **arr;
+	int i, j;
+
+	i = j = 0;
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
@@ -24,7 +27,9 @@ int **alloc_grid(int width, int height)
 		if (arr[i] == NULL)
 		{
 			for (i = i - 1; i >= 0; i--)
+			{
 				free(arr[i]);
+			}
 			free(arr);
 			return (NULL);
 		}
