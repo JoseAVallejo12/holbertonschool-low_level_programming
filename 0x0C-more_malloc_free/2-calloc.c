71c9d6af0a7979974ab70be0547ec34d691aaa6d
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-03-05 15:08:58 -0500

    edit task

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,17 +1,23 @@
 #include "holberton.h"
 
 /**
- *malloc_checked - allocated memory for pointer void
- *@b: size to allocate
- *Return: p
+ *_calloc -allocated memoria for nmeb elemn de zise bytes
+ *@nmemb: number of element in the array
+ *@size: bytes for each position in the array
+ *Return: pointer void
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	void *p;
+	int n;
 
 	p = malloc(nmemb * size);
 	if (p == NULL)
 		return (NULL);
+
+	for (n = 0; n < size; n++)
+		*(p + n) = 0;
+
 	return (p);
 
 }
