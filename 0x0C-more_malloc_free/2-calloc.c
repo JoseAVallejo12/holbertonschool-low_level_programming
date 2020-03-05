7f0b3fd1098d26f7cb98ad00cf98b5277ca91455
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-03-05 15:11:47 -0500

    edit task 1

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,6 +11,9 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	void *p;
 	int n;
 
+	if (nmemb == 0 && size == 0)
+		return (NULL);
+
 	p = malloc(nmemb * size);
 	if (p == NULL)
 		return (NULL);
