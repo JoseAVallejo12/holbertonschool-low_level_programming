562b4f71922525ce09f19647fbf7289f41395094
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-03-09 15:18:28 -0500

    first commit

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ *struct dog - dog propiedades
+ *@name: First member
+ *@age: Second member
+ *@owner: dueno
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
