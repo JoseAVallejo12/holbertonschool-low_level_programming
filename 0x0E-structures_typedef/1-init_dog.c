1440883b5b6c3d25a80d0061bd328d62c994ba5b
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-03-09 16:59:48 -0500

    edit

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stdlib.h>
 
 /**
  *init_dog - inizialice struct data type
@@ -10,7 +11,11 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d == NULL)
+		return;
+
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
 }
+
