7695d00182fadbc6cdde8a23541b7c20a0510ddc
Jose Vallejo <josealfredovallejo25@gmail.com>
2020-03-11 13:31:34 -0700

    create task 2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - funtion
+ * @array: string to process
+ * @size: legeth of array
+ * @cmp: Pointer to funtion
+ * Return: Alway int number
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i, result;
+
+	if ((array != NULL && cmp != NULL) || size > 0)
+	{
+		for (i = 0; i < size; i++)
+		{
+			result = cmp(array[i]);
+			if (result != 0)
+				return (i);
+		}
+	}
+	return (-1);
+}
+
