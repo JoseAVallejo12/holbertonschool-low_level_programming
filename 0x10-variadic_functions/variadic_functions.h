c7464bf928fbbd15afdc32e2cbdf3110c7290525
Jose Vallejo <josealfredovallejo25@gmail.com>
2020-03-14 13:07:55 -0500

    edit header

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,8 +1,8 @@
 #ifndef VARIADIC__FUNCTIONS_H
 #define VARIADIC__FUNCTIONS_H
+
 #include <stdio.h>
 #include <unistd.h>
-#include <stdarg.h>
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
