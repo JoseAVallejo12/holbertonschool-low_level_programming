daeb08d75d80aa83557dc9104ca73190d30f285e
Jose Vallejo <josealfredovallejo25@gmail.com>
2020-03-14 13:24:17 -0500

    edit task 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -22,8 +22,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 			if (i != n - 1)
 				printf("%s ", separator);
 		}
+		
+	printf("\n");
 	}
 	va_end(num);
-	printf("\n");
 
 }
+
