26eb1f5686dd74abd736bcad127c449805ddaf50
Jose Vallejo <josealfredovallejo25@gmail.com>
2020-03-15 08:11:11 -0500

    change to switch funtion

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,53 +1,6 @@
 #include "variadic_functions.h"
 
 /**
- * prt_char - print Character
- * @datos: funtion type variadic
- */
-
-void prt_char(va_list datos)
-{
-	printf("%c", va_arg(datos, int));
-}
-
-/**
- * prt_int - print Integer
- * @datos: funtion type variadic
- */
-
-void prt_int(va_list datos)
-{
-	printf("%d", va_arg(datos, int));
-}
-
-/**
- * prt_float - print float
- * @datos: funtion type variadic
- */
-
-void prt_float(va_list datos)
-{
-	printf("%f", va_arg(datos, double));
-}
-
-/**
- * prt_datos - print String
- * @datos: funtion type variadic
- */
-
-void prt_datos(va_list datos)
-{
-	char *p;
-
-	p = va_arg(datos, char *);
-
-	if (p == NULL)
-		p = "(nil)";
-
-	printf("%s", p);
-}
-
-/**
  * print_all - prts anything.
  * @format: const Pointer const
  * Return: void
@@ -57,28 +10,46 @@ void print_all(const char * const format, ...)
 {
 	unsigned int i, j;
 	va_list datos;
-	char *s = "";
+	char *s = "cifs";
+	char *p;
+	char *space = "";
 
-	f_list  prts[] = {
-		{"c", prt_char},
-		{"i", prt_int},
-		{"f", prt_float},
-		{"s", prt_datos}
-		};
-
-	i = 0;
 	va_start(datos, format);
+	
+	i = 0;
 	while (format != NULL && format[i] != '\0')
 	{
 		j = 0;
 		while (j < 4)
 		{
-			if (format[i] == prts[j].opc[0])
+			if (format[i] == s[j])
 			{
-				printf("%s", s);
-				prts[j].f(datos);
-				s = ", ";
-				break;
+				printf("%s", space);
+				space = ", ";
+				switch(s[j])
+				{
+					case 'c':
+						printf("%c", va_arg(datos, int));
+						break;
+
+					case 'i':
+						printf("%d", va_arg(datos, int));
+						break;
+
+					case 'f':
+						printf("%f", va_arg(datos, double));
+						break;
+
+					case 's':
+						p = va_arg(datos, char *);
+						if (p == NULL) 
+							p = "(nil)";
+						printf("%s", p);
+						break;
+					
+					default: break;
+
+				}
 			}
 			j++;
 		}
