ee083c1f06abd310c54d631631910d3296cb8c22
Jose Vallejo <josealfredovallejo25@gmail.com>
2020-03-24 17:20:59 -0500

    add return in line 55

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -32,7 +32,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	unsigned int i = 0;
 	unsigned int size_list = list_size(head);
 	listint_t *aux = *head;
-	listint_t *new_node;
+	listint_t *new_node = NULL;
 
 	/* validate if list is diferent of NULL */
 	if (head == NULL)
@@ -40,23 +40,20 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	/* create new node of the size the listint_t */
 	new_node = (listint_t *)malloc(sizeof(listint_t));
-
 	/* validate if the size of list and new_node is ok */
 	if (new_node == NULL || idx > size_list)
 	{
 		free(new_node);
 		return (NULL);
 	}
-
 	/* set data a new node */
 	new_node->n = n;
-
 	if (idx == 0)
 	{
 		new_node->next = *head;
 		*head = new_node;
+		return (new_node);
 	}
-
 	while (aux != NULL)
 	{
 		if (i == (idx - 1))
