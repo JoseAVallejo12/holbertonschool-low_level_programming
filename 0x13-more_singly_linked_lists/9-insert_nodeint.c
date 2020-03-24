58418ef7b68054fc019071a78c5cc3f6de701624
Jose Vallejo <josealfredovallejo25@gmail.com>
2020-03-24 13:35:27 -0500

    fix some error

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,68 @@
+#include "lists.h"
+
+/**
+ * listint_len - funtion for length elemnt
+ * @h: single list in
+ * @length number of element an single list linked
+ * Return: number of elemt in the linked lis
+ */
+
+int list_size(listint_t **h)
+{
+	listint_t *aux;
+	int i = 0;
+
+	aux = *h;
+	while (aux != NULL)
+	{
+		i++;
+		aux = aux->next;
+	}
+
+	return (i);
+}
+
+/**
+ *
+ *
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	unsigned int i = 0;
+	unsigned int size_list = list_size(head);
+	listint_t *aux = *head;
+	listint_t *new_node;
+	
+	/* validate if list is diferent of NULL */
+	if (head == NULL)
+		return (NULL);
+
+	/* create new node of the size the listint_t */
+	new_node = (listint_t *)malloc(sizeof(listint_t));
+	
+	/* validate if the size of list and new_node is ok */
+	if (new_node == NULL || idx > size_list)
+		return (NULL);
+
+	if (idx == 0)
+	{
+		new_node->n = n;
+		new_node->next = *head;
+		*head = new_node;
+	}
+
+	while (aux != NULL)
+	{
+		if (i == (idx - 1))
+		{
+			new_node->next = aux->next;
+			new_node->n = n;
+			aux->next = new_node;
+			return (new_node);
+		}
+		aux = aux->next;
+		i++;
+	}
+	return (NULL);
+}
+
