ec0e50680a6ed98f8ce53066d58e8961573ee542
Jose Vallejo <josealfredovallejo25@gmail.com>
2020-03-24 22:25:48 -0500

    create task advance 101

---
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+/**
+ * print_listint_safe - funtion for print single linked list
+ * @head: single list in
+ * @legent of the single list linked and print datas
+ * Return: number of elemt in the linked lis
+ */
+
+size_t print_listint_safe(const listint_t *head)
+{
+	const listint_t *aux = head, *temp = NULL;
+	size_t i = 0;
+
+	if (aux == NULL)
+		exit(98);
+
+	while (aux != NULL)
+	{
+		temp = aux;
+		printf("[%p] %d\n", (void *)aux, aux->n);
+		aux = aux->next;
+		i++;
+
+		if (temp < aux)
+		{
+			printf("-> [%p] %d\n", (void *)head, head->n);
+			break;
+		}
+
+	}
+
+	return (i);
+}
+
