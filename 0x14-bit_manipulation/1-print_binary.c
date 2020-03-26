b7b0db7acc4143a4475bd2f4516c67f18a40751b
Jose Vallejo <josealfredovallejo25@gmail.com>
2020-03-26 13:37:36 -0500

    create file task 1

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+
+/**
+ * print_binary - funtion to print print number bynary
+ * @n: number integer
+ * Return: void
+ */
+void print_binary(unsigned long int n)
+{
+	int i = 0;
+	unsigned long int aux1;
+	unsigned long int aux2 = n;
+
+	/* if n is 0 0r 1 print */
+	if (n < 2)
+	{
+		_putchar((n & 1) + '0');
+		return;
+	}
+
+	while (aux2 > 0)
+	{
+		aux2 = aux2 >> 1;
+		i++;
+	}
+	while (i > 0)
+	{
+		i--;
+		aux1 = n >> i;
+		_putchar((aux1 & 1) + '0');
+	}
+}
