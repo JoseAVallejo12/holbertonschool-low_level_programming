266fbb09e882647b3933eb6a6b1030bcb310e074
Jose Vallejo <josealfredovallejo25@gmail.com>
2020-03-26 16:58:43 -0500

    fix handle error

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -4,17 +4,20 @@
  * set_bit - set bit in index givend
  * @n: pointer to var int
  * @index: index to set value
- * Return: new value inter
+ * Return: 1 if worked or -1 if failure
  */
 
 int set_bit(unsigned long int *n, unsigned int index)
 {
 	int p = *n;
 
+	if (index > 64)
+		return (-1);
+
 	/* find index position and OR operation betwin both */
 	p = ((p | (1 << index)));
 	*n = p;
-
 	/* return new value get operation */
-	return (p);
+	return (1);
+
 }
