32e15abc26ba0a0f111e5a2afaf2ea408cebb1b8
Jose Vallejo <josealfredovallejo25@gmail.com>
2020-03-31 17:33:56 -0500

    create task2

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+
+/**
+ * append_text_to_file - funtion open or create file
+ * @filename: name of the file to open or create
+ * @text_content: string to write in the file
+ * Return: Always a integer
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int file_description, size = 0, fd;
+
+	if (filename == NULL)
+		return (-1);
+
+	/* if O_CREATE is especific must be set permision, */
+	/* O_TRUNC allowed overwrite file, if it exist */
+	file_description = open(filename, O_RDWR | O_APPEND);
+	if (file_description == -1)
+		return (-1);
+
+	/* if not be contex only create empty file */
+	if (text_content != NULL)
+	{
+		for (size = 0; text_content[size]; size++)
+			;
+		fd = write(file_description, text_content, size);
+		if (fd != size)
+			return (-1);
+	}
+	/* not is necesary close file, if all is done return 1 */
+	return (1);
+}
+
