dcf3659d7aaf5f9844f58de13bf667c3eb2152e2
Jose Vallejo <josealfredovallejo25@gmail.com>
2020-03-31 14:56:20 -0500

    task 0 done in the cheker

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,19 +10,24 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fb, read_int;
+	/* best practique is better usar malloc that array */
 	char *buf = (char *)malloc(sizeof(char) * letters);
 
+	/* if failure malloc o the string filename is null return o */
 	if (filename == NULL || buf == NULL)
 		return (0);
 
+	/* if fauilure open file return 0 */
 	fb = open(filename, O_RDONLY);
 	if (fb == -1)
 		return (0);
-
+	
+	/* if fauilure read file return 0 */
 	read_int = read(fb, buf, letters);
 	if (read_int == -1)
 		return (0);
 
+	/* add null caracter for indicate finish of string */
 	buf[read_int] = '\0';
 
 	close(fb);
