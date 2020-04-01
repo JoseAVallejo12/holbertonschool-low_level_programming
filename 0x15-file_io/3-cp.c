19e183b8cb89dab5783dab158019aadf7ff563b7
Jose Vallejo <josealfredovallejo25@gmail.com>
2020-03-31 22:24:24 -0500

    some fixes

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,47 +1,62 @@
 #include "holberton.h"
 #define BF_SIZE 1024
+
+/**
+ * close_file - close file processs
+ * @fd: file description
+ * Return: Always int
+ */
+
+int close_file(int fd)
+{
+	if (close(fd) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		return (-1);
+	}
+	return (0);
+}
+
 /**
  * copy_from_to - funtion copy file to file
  * @name_from_file: name of file source
  * @name_to_file: name file destine
- * @fds: file description
  * Return: Always a integer
  */
 
-int copy_from_to(const char *name_from_file, char *name_to_file, int *fds)
+int copy_from_to(const char *name_from_file, char *name_to_file)
 {
 	int fd_file1, fd_file2, fd_rd, fd_wr;
-	char bufer[BF_SIZE];
-
-	if (name_from_file == NULL || name_to_file == NULL)
-		return (-1);
+	char *bufer[BF_SIZE];
 
 	fd_file1 = open(name_from_file, O_RDONLY);
 	if (fd_file1 == -1)
 		return (98);
 
-	fd_rd = read(fd_file1, bufer, 1024);
-	if (fd_rd == -1)
-		return (98);
-	
 	fd_file2 = open(name_to_file, O_CREAT | O_WRONLY | O_TRUNC, 00664);
 	if (fd_file2 == -1)
 		return (99);
 
-	fd_wr = write(fd_file2, &bufer, BF_SIZE);
-	if (fd_wr != BF_SIZE)
-		return (99);
+	fd_rd = read(fd_file1, bufer, 1024);
+	if (fd_rd == -1)
+	{
+		if (close_file(fd_file1) == -1 || close_file(fd_file2) == -1)
+			return (100);
+		else
+			return (98);
+	}
 
-	if (close(fd_file2) == -1)
+	fd_wr = write(fd_file2, bufer, BF_SIZE);
+	if (fd_wr != BF_SIZE)
 	{
-		*fds = fd_file2;
-		return (100);
+		if (close_file(fd_file1) == -1 || close_file(fd_file2) == -1)
+			return (100);
+		else
+			return (99);
 	}
-	if (close(fd_file1) == -1)
-	{
-		*fds = fd_file1;
+
+	if (close_file(fd_file1) == -1 || close_file(fd_file2) == -1)
 		return (100);
-	}
 
 	return (0);
 }
@@ -56,23 +71,19 @@ int copy_from_to(const char *name_from_file, char *name_to_file, int *fds)
 int main(int ac, char **files)
 {
 	int error = 0;
-	int file_desct;
 
 	if (ac != 3)
 	{
-		dprintf(2, "Usage: cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	error = copy_from_to(files[1], files[2], &file_desct);
+	error = copy_from_to(files[1], files[2]);
 	/* if file_from does not exist, exit with code 98 */
 	if (error == 98)
-		dprintf(2, "Error: Can't read from file %s\n", files[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", files[1]);
 	/* if you can not create or if write fails, exit 99 */
 	if (error == 99)
-		dprintf(2, "Error: Can't write to %s\n", files[2]);
-	if (error == 100)
-		dprintf(2, "Error: Can't close fd %d\n", file_desct);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", files[2]);
 
 	exit(error);
-
 }
