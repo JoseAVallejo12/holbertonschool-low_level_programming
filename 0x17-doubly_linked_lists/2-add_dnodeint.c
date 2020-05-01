05721a104c96f31a548799d32ae9bee070823907
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-05-01 15:38:12 -0500

    update

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -1,7 +1,10 @@
 #include "lists.h"
+
 /**
- *
- *
+ * add_dnodeint - add node front list
+ * @head: head of the linked list
+ * @n: data to save in new node
+ * Return: new node address
  */
 
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
@@ -9,7 +12,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 	dlistint_t *aux = *(head);
 	if (head == NULL)
 		return (NULL);
-			
+
 	dlistint_t *new = malloc(sizeof(dlistint_t ));
 	if (new == NULL)
 		return (NULL);
