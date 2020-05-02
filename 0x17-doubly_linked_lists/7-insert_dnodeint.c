1f2e376a0b926910fa898b194fe5c1cae6a9789d
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-05-02 10:29:56 -0500

    fix task7 ..

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -10,28 +10,37 @@
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	unsigned int i = 0;
-	dlistint_t *t1, *t2 = *h, *new;
+	dlistint_t *node_prev, *node_next = *h, *new;
+
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
+
+	if (!node_next)
+	{
+		new->n = n;
+		new->next = NULL;
+		new->prev = NULL;
+	}
 	/* add node in the front */
 	if (idx == 0)
 		return (add_dnodeint(h, n));
 	/* add node in the index */
-	while (t2->next != NULL)
+	while (node_next->next != NULL)
 	{
 		if (i == idx)
 		{
-			new = malloc(sizeof(dlistint_t));
-			if (new == NULL)
-				return (NULL);
+
 
 			new->n = n;
-			new->next = t2;
-			new->prev = t1;
-			t2->prev = new;
-			t1->next = new;
+			new->next = node_next;
+			new->prev = node_prev;
+			node_next->prev = new;
+			node_prev->next = new;
 			return (new);
 		}
-		t1 = t2;
-		t2 = t2->next;
+		node_prev = node_next;
+		node_next = node_next->next;
 		i++;
 	}
 	/* and node in the end */
