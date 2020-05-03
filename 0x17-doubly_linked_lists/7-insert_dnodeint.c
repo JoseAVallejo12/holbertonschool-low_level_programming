1f4052f5d2df33364d11b8c8078976d5f972f311
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-05-02 19:06:36 -0500

    fix problems task 2...

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -11,7 +11,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
 	unsigned int i = 0;
 	dlistint_t *node_prev, *node_next = *h, *new;
-	/* validate */
+	/* validate linked list head */
 	if (*h == NULL)
 		return (NULL);
 	/* add node in the front */
@@ -37,6 +37,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		node_next = node_next->next;
 		i++;
 	}
+	/* add node in the end */
 	if (i == idx)
 		return (add_dnodeint_end(h, n));
 	return (NULL);
