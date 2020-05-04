ee37c04111eb49342c7a73773c0843d680d01efe
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-05-04 13:29:20 -0500

    create advance task 100

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,55 @@
+#include "task_adv_one.h"
+/**
+ * add - funtions for aritmetic sum
+ * @a: value one
+ * @b: value two
+ * Return: result
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - funtions for aritmetic sub
+ * @a: value one
+ * @b: value two
+ * Return: result
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - funtions for aritmetic mul
+ * @a: value one
+ * @b: value two
+ * Return: result
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - funtions for aritmetic div
+ * @a: value one
+ * @b: value two
+ * Return: result
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - funtions for aritmetic module
+ * @a: value one
+ * @b: value two
+ * Return: result
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
---
+++ 0x18-dynamic_libraries/task_adv_one.h
@@ -0,0 +1,10 @@
+#ifndef TASK_ADV_ONE_H
+#define TASK_ADV_ONE_H
+#include <stdio.h>
+/*  ****prototype advance task 100 ****  */
+int add(int a, int b);
+int sub(int a, int b);
+int mul(int a, int b);
+int div(int a, int b);
+int mod(int a, int b);
+#endif
