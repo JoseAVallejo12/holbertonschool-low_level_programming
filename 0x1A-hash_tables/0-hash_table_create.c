326339d077ba34e82191f3cb2a706641cba2cb8d
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-05-29 11:43:13 -0500

    betty done task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -8,15 +8,15 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_hash = NULL;
-	
+
 	new_hash = malloc(sizeof(hash_table_t));
 	if (new_hash == NULL)
-		return NULL;	
+		return (NULL);
 
 	new_hash->array = malloc(sizeof(hash_node_t) * size);
 	if (new_hash->array == NULL)
-		return NULL;
-	
+		return (NULL);
+
 	memset(new_hash->array, 0, size * sizeof(hash_node_t));
 
 	return (new_hash);
