97d98d428e78bc6fa4fb5d99666b72125fc440c8
josevallejo1984 <josealfredovallejo25@gmail.com>
2020-07-03 14:11:04 -0500

    create files task 0 to 4

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,9 +1,9 @@
 #include"holberton.h"
 
 /**
- *_isupper - evaluate if c is upper case
- *@c: parameter
- *Return: 1 for upper case or 0 otherwise
+ * _isupper - evaluate if c is upper case
+ * @c: parameter
+ * Return: 1 for upper case or 0 otherwise
  */
 
 int _isupper(int c)
