14c70666b19f954d7b7a5e7658be8116a2dbd491
Jose A. Vallejo <54599840+josevallejo1984@users.noreply.github.com>
2020-08-04 21:17:44 -0500

    fix return var

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,7 +9,9 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fb, read_int;
+	int fb;
+	ssize_t read_int, wr;
+
 	/* best practique is better usar malloc that array */
 	char *buf = (char *)malloc(sizeof(char) * letters);
 
@@ -20,17 +22,26 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	/* if fauilure open file return 0 */
 	fb = open(filename, O_RDONLY);
 	if (fb == -1)
+	{
+		free(buf);
 		return (0);
+	}
 	
 	/* if fauilure read file return 0 */
 	read_int = read(fb, buf, letters);
+	close(fb);
 	if (read_int == -1)
+	{
+		free(buf);
 		return (0);
+	}
 
 	/* add null caracter for indicate finish of string */
 	buf[read_int] = '\0';
+	wr = write(STDOUT_FILENO, buf, read_int)
+	if (wr == -1)
+		wr = 0;
+	free(buf)
 
-	close(fb);
-
-	return (write(STDOUT_FILENO, buf, read_int));
+	return (wr);
 }
